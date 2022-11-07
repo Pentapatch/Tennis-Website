ea8bf3f75dfbefbf4233bd7ae12a67ee1fc0474f
Pentapatch <newside-sys@hotmail.com>
2022-11-07 15:57:28 +0100

    initial commit, function done

---
+++ javascript/scroll-to-top.js
@@ -0,0 +1,30 @@
+// Configure
+const minScrollTop = 400;
+
+// Assign the button to a const
+const scrollButton = document.getElementById("scroll-to-top-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    // Check if we are at the bottom of the page
+    if (document.body.offsetHeight - (window.innerHeight + window.scrollY) <= 1) {
+        scrollButton.style.bottom = "4px";
+    } else {
+        scrollButton.style.bottom = "20px";
+    }
+
+    // Check if the user has scrolled down below the set threshold
+    if (document.body.scrollTop >= minScrollTop || document.documentElement.scrollTop >= minScrollTop) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
